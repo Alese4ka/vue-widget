a39c5c934d5f789a324698d84fe278432c75fcb6
Alesia Branavitskaya <alesun4ik@gmail.com>
2023-02-05 14:03:34 +0300

    use LimitChunkCountPlugin

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,14 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
-const webpack = require('webpack');
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { optimize } from 'webpack';
 
-const assetsDir = 'assets';
-module.exports = {
-  assetsDir,
-  configureWebpack: {
-    output: {
-      filename: `${assetsDir}/[name].js`,
-      chunkFilename: `${assetsDir}/[name].js`,
-    },
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1,
-      }),
-    ],
-  },
-  chainWebpack: (config) => {
-    config.optimization.delete('splitChunks');
-
-    if (config.plugins.has('extract-css')) {
-      const extractCSSPlugin = config.plugin('extract-css');
-      // eslint-disable-next-line no-unused-expressions, operator-linebreak
-      extractCSSPlugin &&
-        extractCSSPlugin.tap(() => [
-          {
-            filename: `${assetsDir}/[name].css`,
-            chunkFilename: `${assetsDir}/[name].css`,
-          },
-        ]);
-    }
-  },
+export const filenameHashing = false;
+export const configureWebpack = {
+  plugins: [
+    new optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+  ],
 };
+export function chainWebpack(config) {
+  config.optimization.delete('splitChunks');
+}
