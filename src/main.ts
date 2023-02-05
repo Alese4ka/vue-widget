63206717de5758629798f9abac3d5a65b2994d50
Alesia Branavitskaya <alesun4ik@gmail.com>
2023-02-05 14:00:24 +0300

    set up weather-widget

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import Vue, { createApp } from 'vue';
+import { createApp, defineCustomElement } from 'vue';
 import { createVuetify } from 'vuetify';
 // eslint-disable-next-line import/extensions, import/no-extraneous-dependencies
 import vueCustomElement from 'vue-custom-element';
-// eslint-disable-next-line import/extensions
+// eslint-disable-next-line import/extensions, import/no-unresolved
 import App from './App.vue';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'document-register-element/build/document-register-element';
 
 const vuetify = createVuetify();
+const weatherWidget = defineCustomElement(App);
 
+customElements.define('weather-widget', weatherWidget);
 createApp(App).use(vuetify).use(vueCustomElement).mount('#app');
